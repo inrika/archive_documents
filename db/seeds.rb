24adaed30885722910458ad240218e0eb7dd236a
inrika <Irina_Schetinina@mail.ru>
2017-03-24 16:22:05 +0300

    devise-username

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(username: 'admin', email: '123@example.com',password: 'password', password_confirmation: 'password')
