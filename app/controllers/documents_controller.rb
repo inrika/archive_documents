4600791a602e6879c2d7a7bc9673c5113b8416e5
inrika <Irina_Schetinina@mail.ru>
2017-04-13 16:06:38 +0300

    load_document into model

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -60,27 +60,8 @@ class DocumentsController < ApplicationController
  end
 
  def import_documents
-   require 'csv'
-   CSV.foreach('C:\Sites\archive_documents\bd2.csv',  col_sep: ';', headers: true) do |row|
-#   CSV.foreach('C:\Sites\archive_documents\adm1.csv', col_sep: ';',headers: true) do |row|
-      @category = Category.find(params[:category_id])
-      @category = Category.find(params[:category_id])
-      @document= Document.new()
-      @document.doc_num = row[1]
-      @document.doc_date = row[2]
-      @document.doc_type = row[0]
-      @document.content = row[3]
-      @document.branch = row[4]
-      @document.geography = row[5]
-      @document.office = row[6]
-      @document.fund = row[7]
-      @document.inventory = row[8]
-      @document.storage_unit = row[9].to_i
-      @document.page = row[10]
-      @document.note = row[11]
-      @category.documents << @document
-      @document.save
-   end
+   @category = Category.find(params[:category_id])
+   @category.add_documents
  end
 
  def load_reference
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,25 @@
 class Category < ApplicationRecord
   has_many :documents
+
+  def add_documents
+    require 'csv'
+    CSV.foreach('C:\Sites\archive_documents\bd2.csv',  col_sep: ';', headers: true) do |row|
+    #   CSV.foreach('C:\Sites\archive_documents\adm1.csv', col_sep: ';',headers: true) do |row|
+      document= Document.new()
+      document.doc_num = row[1]
+      document.doc_date = row[2]
+      document.doc_type = row[0]
+      document.content = row[3]
+      document.branch = row[4]
+      document.geography = row[5]
+      document.office = row[6]
+      document.fund = row[7]
+      document.inventory = row[8]
+      document.storage_unit = row[9].to_i
+      document.page = row[10]
+      document.note = row[11]
+      documents << document
+      document.save
+    end
+  end
 end
